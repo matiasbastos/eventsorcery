bd1fd61e1301522556b8dc416e7be9f246eae7c9
Matias Bastos <matias.bastos@xcaliber.com>
2018-12-04 01:33:31 +0100

    basic setup

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # eventsorcery
 Simple Event Sourcing ORM for Python
+```
+pip install -e git+https://github.com/matiasbastos/eventsorcery.git#egg=project[sqlalchemy]
+```
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='eventsorcery',
+      version='0.1',
+      description='Simple and pythonic eventsourcing ORM.',
+      url='http://github.com/matiasbastos/eventsorcery',
+      author='Matias Bastos, Imre Guzmics',
+      author_email='matias.bastos@gmail.com, gizmo.mac@gmail.com',
+      license='MIT',
+      packages=[],
+      extras_require = {
+        'sqlalchemy':  ['SQLAlchemy==1.2.14', 'pudb==2018.1']
+      },
+      zip_safe=False)
